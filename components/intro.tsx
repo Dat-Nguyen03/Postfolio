28bdbe7eee8ec5a70cd66a9fe2b37bbca7d24594
Dat Nguyen <datnguyen01122003@gmail.com>
2025-01-20 19:40:27 +0700

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
         <span className="font-bold">front-end developer</span> with{" "}
         <span className="font-bold">1 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js), React Native</span>.
+        <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
       <motion.div
